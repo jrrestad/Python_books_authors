57b2b201d9f78d659a9e09e49bb400ac2331ab4e
Jonas Restad <jrrestad@gmail.com>
2020-12-06 21:01:42 -0800

    Added book and author class

--- app_books_authors/models.py
+++ app_books_authors/models.py
@@ -2,3 +2,19 @@ from django.db import models
 from app_login_registration.models import User
 
 # Create your models here.
+
+class Book(model.models):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    uploaded_by = models.ForeignKey(User, related_name="books_uploaded")
+    users_who_like = models.ManyToManyField(User, related_name="liked_books")
+    authors = models.ManyToManyField(Author, related_name="books")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Author(model.models):
+    firstName = models.CharField(max_length=50)
+    lastName = models.CharField(max_length=50)
+    # books = models.ManyToManyField(Book, related_name="authors")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
